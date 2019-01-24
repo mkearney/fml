07ba4b358e52c33645b5faf8a4912d5e2b0db0ce
Michael W. Kearney <kearneymw@missouri.edu>
2019-01-24 10:30:52 -0600

    Test roflzomg

--- tests/testthat/test-here.R
+++ tests/testthat/test-here.R
@@ -1,11 +1,11 @@
 context("test-here")
 
 test_that("here works", {
-  expect_true(grepl("testthat/file.R", here("file.R")))
-  expect_true(grepl("testthat/R/file.R", here("R/file.R")))
+  expect_true(grepl("file.R", here("file.R")))
+  expect_true(grepl("R/file.R", here("R/file.R")))
   expect_true(grepl("testthat/test-here.R",
-    here("tests", "testthat", "test-here.R")))
+    here("testthat", "test-here.R")))
   expect_true("test-here.R" %in% file_name(list_files()))
-  expect_true("tests" %in% file_name(dir_name(list_dirs(".."))))
+  expect_equal("test", file_name(dir_name(fp("test", "okay"))))
   #expect_true("DESCRIPTION" %in% file_name(list_files("..")))
 })
